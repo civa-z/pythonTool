18199b9746ddfc591b810a857b2d3d69147c1880
zhangwenming <wenming.zhang@sony.com>
2017-06-22 19:31:45 +0800

    2

--- tensorflow_variable.py
+++ tensorflow_variable.py
@@ -9,10 +9,12 @@ with graph.as_default():
     D3rnn_cell = tf.nn.rnn_cell.BasicRNNCell(num_units=128, input_size=None, activation=tf.nn.tanh)
 
     with tf.variable_scope("x1"):
-        outputsi1, states1 = tf.nn.rnn(D3rnn_cell, [x1], dtype=tf.float32)
+        outputs1, states1 = tf.nn.rnn(D3rnn_cell, [x1], dtype=tf.float32)
+        print(outputs1[0].name)
 
     with tf.variable_scope("x1", reuse=True):
-        outputsi2, states2 = tf.nn.rnn(D3rnn_cell, [x2], dtype=tf.float32)
+        outputs2, states2 = tf.nn.rnn(D3rnn_cell, [x2], dtype=tf.float32)
+        print(outputs2[0].name)
 
 with tf.Session(graph=graph) as session:
    tf.initialize_all_variables().run(); 
